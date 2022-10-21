f9dcbff4528cbfac1db0cff56cfdc5086ccd5b2b
nickan2c <nickan2c@gmail.com>
2022-10-21 17:17:40 +0100

    Implement functionality

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    num = 3
+    if number_of_primes < 1:
+        raise ValueError
+    else:
+        while len(list) < number_of_primes:
+            if num % 2 != 0:
+                for i in range(2, num):
+                    if (num % i) == 0:
+                        break
+                else:
+                    list.append(num)
+            num += 1
+
     return list
